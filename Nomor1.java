b220f8d00d694f85be5a6407988d1712f37daf20
egafiandra <egafiandrapratama@gmail.com>
2024-12-12 10:13:39 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,15 +1,30 @@
 import java.util.Scanner;
 
-public class Nomor1 {
-
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-
-        /* Pekerjaan anda berakhir sini */
-
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
+public class FizzBuzz {
+  
+    public static void cetakFizzBuzz(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } 
+            else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } 
+            else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } 
+            else {
+                System.out.print(i + " ");
+            }
+        }
     }
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukan nilai N : ");
+        int n = scanner.nextInt();
+        cetakFizzBuzz(n);
+        scanner.close();
+    }
 }
