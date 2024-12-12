f0a922f7238982b5607e2eeef8d3f6f7b8871050
egafiandra <egafiandrapratama@gmail.com>
2024-12-12 10:43:12 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -2,35 +2,47 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nomor2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String[] namaBuku = {"Sherlock Holmes", "Arsene Lupin", "Game of Throne", "Psychology of Money"};
         int[] hargaBuku = {350000, 450000, 500000, 95000};
-
+        
         System.out.println("Daftar Buku:");
         for (int i = 0; i < namaBuku.length; i++) {
-            System.out.println((i + 1) + ". " + namaBuku[i] + " " + hargaBuku[i]);
+            System.out.println((i + 1) + ". " + namaBuku[i] + " - Rp " + hargaBuku[i]);
         }
-
+        
         ArrayList<Integer> hargaBelanja = new ArrayList<>();
+        
         while (true) {
             System.out.print("Masukan pilihan buku [1/2/3/4]: ");
             int pilihan = input.nextInt();
+            
+            // Validasi input pilihan buku
+            if (pilihan < 1 || pilihan > namaBuku.length) {
+                System.out.println("Pilihan buku tidak valid!");
+                continue;
+            }
+            
             hargaBelanja.add(hargaBuku[pilihan - 1]);
+            
             System.out.print("Input lagi (Y/N)?: ");
             String jawaban = input.next();
+            
             if (jawaban.equalsIgnoreCase("N")) {
                 break;
             }
         }
+        
         int totalHarga = hitungTotalHarga(hargaBelanja);
         int voucherBelanja = hitungVoucherBelanja(totalHarga);
-        System.out.println("Total voucher belanja = " + voucherBelanja);
-        System.out.println("Total yang harus dibayar = " + totalHarga);
-        System.out.println(Nomor2.class.getDeclaredMethods().length); 
+        
+        System.out.println("Total voucher belanja = Rp " + voucherBelanja);
+        System.out.println("Total yang harus dibayar = Rp " + (totalHarga - voucherBelanja));
+        
         input.close();
     }
-
+    
     public static int hitungTotalHarga(ArrayList<Integer> hargaBelanja) {
         int totalHarga = 0;
         for (int harga : hargaBelanja) {
@@ -38,6 +50,7 @@ public class Nomor2 {
         }
         return totalHarga;
     }
+    
     public static int hitungVoucherBelanja(int totalHarga) {
         if (totalHarga < 200000) {
             return 0;
